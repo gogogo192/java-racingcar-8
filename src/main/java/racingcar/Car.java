4e9a371336db7faf4f8d6cc8f63977ab13a162df
gogogo192 <gogogo192@naver.com>
2025-10-24 13:35:55 +0900

    feat: Complete Car class with name and movement logic

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,4 +1,26 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Car {
+    private String name;
+    private int position;
+
+    public Car(String name){
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(int number){
+        number = Randoms.pickNumberInRange(0, 9);
+        if(number>4){
+            this.position++;
+        }
+    }
+    public String getName(){
+        return name;
+    }
+    public int getPosition(){
+        return position;
+    }
 }
